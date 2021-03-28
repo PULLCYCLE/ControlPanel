1575b7b0310cbd0ed8470d8028a53958659078e3
PULLCYCLE <Ivanovich.morkva@yandex.ru>
2021-03-28 21:57:22 +0300

    +

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,27 @@
-import {Component, OnInit} from '@angular/core'
-import {Todo, TodosService} from './todos.service'
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  todos: Todo[] = []
-  loading = false
-  todoTitle = ''
-  error = ''
+  img = 'https://www.phonedo32.ru/wp-content/uploads/2017/04/Google_-G-_Logo.svg_.png'
 
-  constructor(private todosService: TodosService) {}
+  inputValue = ''
 
-  ngOnInit() {
-    this.fetchTodos()
+  title = ''
+
+  constructor() {
+    setTimeout( () => {
+      this.img = 'https://yt3.ggpht.com/a/AATXAJxbA17CO-Rxgi4a5DzJ1VatgTDlcEVn1XVmYhJJ=s900-c-k-c0xffffffff-no-rj-mo'
+    }, 5000)
   }
 
-  addTodo() {
-    if (!this.todoTitle.trim()) {
-      return
-    }
-
-    this.todosService.addTodo({
-      title: this.todoTitle,
-      completed: false
-    }).subscribe(todo => {
-      this.todos.push(todo)
-      this.todoTitle = ''
-    })
-  }
-
-  fetchTodos() {
-    this.loading = true
-    this.todosService.fetchTodos()
-      .subscribe(todos => {
-        this.todos = todos
-        this.loading = false
-      }, error => {
-        this.error = error.message
-      })
-  }
-
-  removeTodo(id: number) {
-    this.todosService.removeTodo(id)
-      .subscribe(() => {
-        this.todos = this.todos.filter(t => t.id !== id)
-      })
-  }
-
-  completeTodo(id: number) {
-    this.todosService.completeTodo(id).subscribe(todo => {
-      this.todos.find(t => t.id === todo.id).completed = true
-    })
+  onInput(event?: any) {
+    console.log('Event', event)
+    this.inputvalue = event.target.value
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,6 @@ import {NgModule, Provider} from '@angular/core'
 
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
-import {AuthInterceptor} from './auth.interceptor'
-
-const INTERCEPTOR_PROVIDER: Provider = {
-  provide: HTTP_INTERCEPTORS,
-  useClass: AuthInterceptor,
-  multi: true
-}
 
 @NgModule({
   declarations: [
@@ -18,10 +10,8 @@ const INTERCEPTOR_PROVIDER: Provider = {
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule
+    FormsModule
   ],
-  providers: [INTERCEPTOR_PROVIDER],
   bootstrap: [AppComponent]
 })
 export class AppModule {
